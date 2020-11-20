187ca5b4aa713bbaf8149f1da3cacd1726511e39
Gabriel_Genelhoud <genelha17@gmail.com>
2020-11-20 23:52:39 +0000

    Applying MVC approach and inserting register screen on button on click.

---
+++ src/login/Controller.java
@@ -0,0 +1,61 @@
+
+package login;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JOptionPane;
+
+
+public class Controller implements ActionListener {
+    
+    View view;
+    Model model;
+    
+    public Controller(){
+        this.view = new View(this);
+        this.model = new Model();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        
+        if(e.getSource() == view.loginButton){
+        String un = view.getUsername();
+        String pw = view.getPassword();
+        System.out.println("Username: " + un + " , Password: " + pw);
+        
+        User user = new User(un, pw);
+        boolean result = model.login(user);
+               
+        if((result)){
+            JOptionPane.showMessageDialog(view.success, "Logged in successfuly!!!");
+        }
+        else{
+            JOptionPane.showMessageDialog(view.success, "Wrong Credentials");
+        }
+        }
+        if (e.getSource() == view.registerButton) {
+            
+            registerScreen open = new registerScreen();
+            open.setVisible(true);
+        }
+    }    
+    
+   public static class exitbuttonListener implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            
+            System.exit(0);
+        }
+   }
+}
+  
+    
+    
+    
+    
+
+  
+    
+
