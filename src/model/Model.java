5c626eb0f1cc97f4ab0afd1d484c45fe83f8c135
BacanaDev <bacana.vip@gmail.com>
2020-11-26 14:30:27 +0000

    I add it into model package

---
+++ src/model/Model.java
@@ -0,0 +1,63 @@
+package model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import controller.Controller;
+import login.User;
+
+public class Model {
+    
+    Controller controller;
+
+   
+    public boolean login(User user){
+        
+        boolean result = false;
+        
+        try {
+
+            String dbServer = "jdbc:mysql://apontejaj.com:3306/Gabriel_2019386?useSSL=false";
+            String dbUser = "Gabriel_2019386";
+            String dbPassword = "2019386";
+            String query = "SELECT * FROM user WHERE username = '" + user.getUn() + "' AND password = '" + user.getPw()+ "';";
+
+            // Get a connection to the database
+            Connection conn = DriverManager.getConnection(dbServer, dbUser, dbPassword);
+
+            // Get a statement from the connection
+            Statement stmt = conn.createStatement();
+
+            // Execute the query
+            ResultSet rs = stmt.executeQuery(query);
+
+            // Loop through the result set
+            if (rs.next()) {
+                result = true;
+            }
+
+            // Close the result set, statement and the connection
+            rs.close();
+            stmt.close();
+            conn.close();
+        } catch (SQLException se) {
+            System.out.println("SQL Exception:");
+
+            // Loop through the SQL Exceptions
+            while (se != null) {
+                System.out.println("State  : " + se.getSQLState());
+                System.out.println("Message: " + se.getMessage());
+                System.out.println("Error  : " + se.getErrorCode());
+
+                se = se.getNextException();
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        
+        return result;
+        
+    }
+}
