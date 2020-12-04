fb3c50df4374ba10c04a0bcec6f794e3de541286
BacanaDev <bacana.vip@gmail.com>
2020-12-04 08:51:06 +0000

    Main Frame now has the datapicker and calendar working.

--- src/view/MainFrame.java
+++ src/view/MainFrame.java
@@ -9,6 +9,7 @@ import javax.swing.*;
 import java.awt.*;
 import javax.swing.border.LineBorder;
 import javax.swing.ImageIcon;
+import datapicker.MainWindow;
 
 /**
  *
@@ -16,11 +17,15 @@ import javax.swing.ImageIcon;
  */
 public class MainFrame extends JFrame {
     
-    public MainFrame(String Title){
-    this.setTitle("Grafton Barber");
+   private final MainWindow mw;
+            
+    public MainFrame(String title){
+        
+    this.setTitle(title);
     this.setSize(700,550);
     this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     this.setLocationRelativeTo(null);
+    this.setResizable(false);
     
     JButton Bookings = new JButton("Bookings");
     JButton Feedback = new JButton("Feedback");
@@ -28,22 +33,28 @@ public class MainFrame extends JFrame {
     JButton Submit = new JButton("Submit");
     JButton Close = new JButton("Close");
     
+    
     Container mainContainer = this.getContentPane();
     mainContainer.setLayout(new BorderLayout (8,6));
     mainContainer.setBackground(new Color(212, 198, 94));
-    this.getRootPane().setBorder(BorderFactory.createMatteBorder(4,4,4,4, new Color(153, 120, 92)));
+    //this.getRootPane().setBorder(BorderFactory.createMatteBorder(4,4,4,4, new Color(153, 120, 92)));
     
     
     //LOGO BARBER
     ImageIcon barberLogo = new ImageIcon ("logo-barbershop.png");
     JLabel labelBarber = new JLabel (barberLogo);
+    labelBarber.setBounds(20, 10, 65, 75);
     
     //Top Panel
     JPanel topPanel = new JPanel();
     topPanel.setBorder(new LineBorder(new Color(169, 140, 7),3));
-    topPanel.setBackground(new Color(153, 120, 92));
-    topPanel.setLayout(new FlowLayout(FlowLayout.RIGHT, 250,1));
+    topPanel.setBackground(new Color(153, 120, 92)); //new Color(153, 120, 92)
+    topPanel.setLayout(null);//new FlowLayout(FlowLayout.RIGHT, 250,1)
+    topPanel.setPreferredSize(new Dimension(650,100));
     mainContainer.add(topPanel, BorderLayout.NORTH);
+    //  SET BUTTON DIM & POS
+    //Close.setPreferredSize(new Dimension(50,30));
+    Close.setBounds(600,50,70,30);
     topPanel.add(labelBarber);
     topPanel.add(Close);
     
@@ -56,25 +67,35 @@ public class MainFrame extends JFrame {
     lmidPanel.setBackground(new Color(153, 120, 92));
     
     
-    // LEFT GRID LAYOUT // MENU NUTTONS
+    // LEFT GRID LAYOUT // MENU BUTTONS
     JPanel lGridPanel = new JPanel();
     lGridPanel.setLayout(new GridLayout(2,1,5,5));
+    lGridPanel.setPreferredSize (new Dimension (100,100)); // set the sise of each panel container
     lGridPanel.setBackground(new Color(153, 120, 92));
     lGridPanel.add(Bookings);
     lGridPanel.add(Feedback);
           
-    // -->> MIDDLE CENTER PANEL <<--- //
-    
-    JLabel centerContPanel = new JLabel ("Center Container", SwingConstants.CENTER);
-    centerContPanel.setBorder(new LineBorder(new Color (169, 140, 7),3));
-    
-    // ADD MIDDLE PANELLS INTO MAIN CONTAINER
+    // ADD MIDDLE LEFT PANELLS INTO MAIN CONTAINER
     lmidPanel.add(lGridPanel);
     mainContainer.add(lmidPanel, BorderLayout.WEST);
+    
+    
+    
+    // -->> MIDDLE CENTER PANEL <<--- //
+    
+    JPanel centerContPanel = new JPanel ();
+    centerContPanel.setBorder(new LineBorder(new Color (169, 140, 7),3));
+    centerContPanel.setLayout(new FlowLayout());//new BorderLayout()
+    centerContPanel.setBackground(new Color(153, 120, 92));
     mainContainer.add(centerContPanel);
     
+    //---- SUB PANELS  ------
+    mw = new MainWindow();
+    centerContPanel.add(mw);
+    //centerContPanel.add(tp2, BorderLayout.EAST);
+
     // -->> RIGHT CENTER PANEL
-       JPanel rmidPanel = new JPanel ();
+    JPanel rmidPanel = new JPanel ();
     rmidPanel.setBorder(new LineBorder(new Color(169, 140, 7), 3));
     rmidPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
     rmidPanel.setBackground(new Color(153, 120, 92));
@@ -94,10 +115,12 @@ public class MainFrame extends JFrame {
     
     }
     public static void main(String[] args) {
-        MainFrame barberApp = new MainFrame("Barber App");
+        MainFrame barberApp = new MainFrame("Grafton Barber");
         barberApp.setVisible(true);
     }
     
 }
 
+
+//Action listener "mw" from MainWondow/Datapicker
 
